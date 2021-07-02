057ceae5b47727e1f63a5177f1f5e96856060c5e
prabhakar1828 <singhisking63915@gmail.com>
2021-07-02 22:25:26 +0530

    program

---
+++ server_client.py
@@ -0,0 +1,8 @@
+import socket
+ob=socket.socket()
+ob.bind(('localhost',2301))
+ob.listen(4)
+clientobject,add=ob.accept()
+print("server is ready to accept connection")
+print('connected with this address',add)
+gotmsg=clientobject.recv(1024)
